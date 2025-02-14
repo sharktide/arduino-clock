e88c51ff12e3a8f95d2b50f0b04e3c2effaae6fc
sharktide <meherrihaan@gmail.com>
2025-02-14 12:06:55 -0500

    Update pinmappings.h

--- pinmappings.h
+++ pinmappings.h
@@ -1,15 +1,14 @@
 #ifndef PIN_MAPPINGS_H
 #define PIN_MAPPINGS_H
 
-// Pin assignments for the DHT sensor and LCD on the Arduino Mega 2560
-const int DHT_SENSOR_PIN = 5;  // Pin for the DHT11 sensor
-
-// LCD pins standard 6-pin configuration 
+const int receiver = 4;
+const int DHT_SENSOR_PIN = 5;
 const int LCD_RS = 7;
 const int LCD_ENABLE = 8;
 const int LCD_D4 = 9;
 const int LCD_D5 = 10;
 const int LCD_D6 = 11;
 const int LCD_D7 = 12;
+const int lamp = 22;
 
 #endif
